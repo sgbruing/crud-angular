6153da89f9139cc4d67c6b07a11bd14234a17fea
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-01-18 21:50:26 -0300

    Add nav component

--- frontend/src/app/components/template/nav/nav.component.ts
+++ frontend/src/app/components/template/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component({ //Decorator
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
