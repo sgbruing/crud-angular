d5f0b583288a875efd8ede7eebab9f942b2bcffc
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-01-22 23:43:41 -0300

    Add first structural directive

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,9 +16,10 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { CarCrudComponent } from './views/car-crud/car-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, CarCrudComponent, RedDirective],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, CarCrudComponent, RedDirective, ForDirective],
   imports: [
     BrowserModule,
     AppRoutingModule,
