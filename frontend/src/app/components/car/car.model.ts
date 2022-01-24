f038455444afb0f984defc97e09b2c9101a0bd6e
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-01-24 20:36:16 -0300

    Create car interface

---
+++ frontend/src/app/components/car/car.model.ts
@@ -0,0 +1,10 @@
+export interface Car {
+    id: number
+    name: string
+    year: number
+    color: string
+    Horsepower: number
+    cylinders: number
+    displacements: number
+    price: number
+}
