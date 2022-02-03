f8d75b5ee45f95dc074025acad9b384521c76bce
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-02-02 23:05:01 -0300

    add actions column

--- frontend/src/app/components/car/car-read/car-read.component.ts
+++ frontend/src/app/components/car/car-read/car-read.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class CarReadComponent implements OnInit {
 
   cars: Car[]
-  displayedColumns = ['id', 'name', 'year', 'color', 'horsepower', 'cylinders', 'engine_capacity', 'price']
+  displayedColumns = ['id', 'name', 'year', 'color', 'horsepower', 'cylinders', 'engine_capacity', 'price', 'action']
 
   constructor(private CarService: CarService) { }
 
