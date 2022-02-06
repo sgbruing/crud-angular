4f33177f7b776de8e254a9ca910ebfbff6b5077a
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-02-05 23:35:50 -0300

    css update form

--- frontend/src/app/components/car/car-update/car-update.component.ts
+++ frontend/src/app/components/car/car-update/car-update.component.ts
@@ -1,3 +1,4 @@
+import { Car } from './../car.model';
 import { Router } from '@angular/router';
 import { CarService } from './../car.service';
 import { Component, OnInit } from '@angular/core';
@@ -8,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-update.component.css']
 })
 export class CarUpdateComponent implements OnInit {
+
+  car: Car = {
+    name: "Chevrolet Celta",
+    year: 2004,
+    color: "Azul Santorini",
+    horsepower: 77,
+    cylinders: 4,
+    engine_capacity: 1.0,
+    price: 13.0
+  }
 
   constructor(private carService: CarService, private router: Router) { }
 
