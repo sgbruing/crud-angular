7a3ad70e39cae7bb9fd3d18c24e1b7b630acb91a
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-02-13 23:38:44 -0300

    car delete component form

--- frontend/src/app/components/car/car-delete/car-delete.component.ts
+++ frontend/src/app/components/car/car-delete/car-delete.component.ts
@@ -1,3 +1,5 @@
+import { CarService } from './../car.service';
+import { Car } from './../car.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarDeleteComponent implements OnInit {
 
-  constructor() { }
+  car: Car
+
+  constructor(private carService: CarService) { }
 
   ngOnInit(): void {
+    const id = '1'
+    this.carService.readById(id).subscribe(car => {
+      this.car = car
+    })
+  }
+
+  deleteCar(): void {
+
+  }
+
+  cancel(): void {
+
   }
 
 }
