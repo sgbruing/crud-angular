023237c2a9f13f91d6622a316a81ff436e5bc833
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-02-14 23:55:01 -0300

    style error message

--- frontend/src/app/components/car/car.service.ts
+++ frontend/src/app/components/car/car.service.ts
@@ -13,12 +13,12 @@ export class CarService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top",
-      panelClass: ['msg-success']
+      panelClass: isError ?  ['msg-error'] : ['msg-success']
     })
   }
 
