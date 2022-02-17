35d38b71385fd52b03936b317f0ede5fc41baed4
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-02-17 00:00:48 -0300

    Header icons and title dynamic working!

--- frontend/src/app/views/car-crud/car-crud.component.ts
+++ frontend/src/app/views/car-crud/car-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
@@ -8,8 +9,12 @@ import { Router } from '@angular/router'
 })
 export class CarCrudComponent implements OnInit {
 
-  constructor(private router: Router) { 
-
+  constructor(private router: Router, private headerService: HeaderService) { 
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'directions_car',
+      routeUrl: '/cars'
+    }
   }
 
   ngOnInit(): void {
